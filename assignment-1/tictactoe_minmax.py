71cf13f9aabf1b3a9cb93590c9ba5ae455f1625c
Leon <doncorleone142857@gmail.com>
2023-10-13 20:25:09 +0700

    modified again from scratch

--- assignment-1/tictactoe_minmax.py
+++ assignment-1/tictactoe_minmax.py
@@ -1,141 +1,112 @@
-import math
-import copy
+# Initialize an empty 3x3 game board
+board = [[' ' for _ in range(3)] for _ in range(3)]
 
-def check_winner(state):
-    # check rows
+# Function to print the game board
+def print_board(board):
+    for row in board:
+        print(' | '.join(row))
+        print('-' * 9)
+
+# Function to check if the game is over (win or tie)
+def is_game_over(board):
+    # Check rows, columns, and diagonals for a win
     for i in range(3):
-        if state[i][0] == state[i][1] == state[i][2] and state[i][0] != 0:
-            return state[i][0]
+        if board[i][0] == board[i][1] == board[i][2] != ' ':
+            return board[i][0]
+        if board[0][i] == board[1][i] == board[2][i] != ' ':
+            return board[0][i]
+    if board[0][0] == board[1][1] == board[2][2] != ' ':
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] != ' ':
+        return board[0][2]
+    # Check for a tie
+    if all(board[i][j] != ' ' for i in range(3) for j in range(3)):
+        return 'Tie'
+    return None
 
-    # check columns
-    for j in range(3):
-        if state[0][j] == state[1][j] == state[2][j] and state[0][j] != 0:
-            return state[0][j]
-
-    # check diagonals
-    if state[0][0] == state[1][1] == state[2][2] and state[0][0] != 0:
-        return state[0][0]
-    if state[0][2] == state[1][1] == state[2][0] and state[0][2] != 0:
-        return state[0][2]
-
-    return 0
-
-def check_tie(state):
-    for i in range(3):
-        for j in range(3):
-            if state[i][j] == 0:
-                return False
-    return True
-
-def terminal_node(state):
-    result = check_winner(state)
-    if result != 0:
-        return {"gameover": True, "result": result}
-    if check_tie(state):
-        return {"gameover": True, "result": 0}
-    return {"gameover": False, "result": 0}
-
-def expand_state(state):
-    children = []
-    for i in range(3):
-        for j in range(3):
-            if state[i][j] == 0:
-                child = [i, j]
-                children.append(child)
-    return children
-
-def minimax(state, depth, isMaxPlayer):
-    if depth == 0 or terminal_node(state)["gameover"]:
-        return terminal_node(state)["result"], None
-
-    if isMaxPlayer:
-        v_max = -math.inf
-        best_move = None
-        for pos in expand_state(state):
-            child = copy.deepcopy(state)
-            child[pos[0]][pos[1]] = 1
-            v, _ = minimax(child, depth - 1, not isMaxPlayer)
-            if v > v_max:
-                v_max = v
-                best_move = pos
-        return v_max, best_move
-
-    else:
-        v_min = math.inf
-        best_move = None
-        for pos in expand_state(state):
-            child = copy.deepcopy(state)
-            child[pos[0]][pos[1]] = -1
-            v, _ = minimax(child, depth - 1, not isMaxPlayer)
-            if v < v_min:
-                v_min = v
-                best_move = pos
-        return v_min, best_move
-
-def output_state(state):
-    print("This is the state:")
-    for row in state:
-        print(row)
-
-def player_ply(depth):
-    def get_valid_input(prompt):
-        while True:
-            user_input = input(prompt)
-            if user_input.isdigit() and 0 <= int(user_input) <= 2:
-                return int(user_input)
-            else:
-                print("Invalid input. Please input again.")
-
-    def notify_player(state):
-        output_state(state)
-        print("Please input your move in x-coordinate and y-coordinate.")
-        print("The range of the input is (0-2), (0-2). For example, (0,0) is the top left corner.")
-
+# Function to make a player's move
+def make_move(player, board):
     while True:
-        notify_player(state)
+        try:
+            row, col = map(int, input(f"Enter row and column (0-2) for player {player} (e.g., 0 1): ").split())
+            if board[row][col] == ' ':
+                board[row][col] = player
+                break
+            else:
+                print("Invalid move. Try again.")
+        except (ValueError, IndexError):
+            print("Invalid input. Please enter valid row and column.")
 
-        move_x = get_valid_input("Please input the x-coordinate: ")
-        move_y = get_valid_input("Please input the y-coordinate: ")
+# Function to simulate the bot's move using minimax
+def bot_move(board):
+    best_score = -float("inf")
+    best_move = None
 
-        if state[move_y][move_x] != 0:
-            print("Invalid move, cell is occupied. Please try again.")
-        else:
-            state[move_y][move_x] = 1
-            print(f"Player chooses the move {move_x}, {move_y}")
-            break
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == ' ':
+                board[i][j] = 'O'  # Assume the bot's move
+                score = minimax(board, 0, False)
+                board[i][j] = ' '  # Undo the move
 
-    return depth - 1
+                if score > best_score:
+                    best_score = score
+                    best_move = (i, j)
 
-def bot_ply(depth):
-    v, move = minimax(state, depth, isMaxPlayer)
-    output_state(state)
-    result = "a tie" if v == 0 else "a win" if v == 1 else "a defeat"
-    print(f"The bot chooses {move[1]}, {move[0]} as the current move, with expected result: {result}")
-    state[move[0]][move[1]] = -1
+    if best_move:
+        board[best_move[0]][best_move[1]] = 'O'  # Make the best move for the bot
 
-    return depth - 1
+# Minimax algorithm to evaluate the game state
+def minimax(board, depth, is_maximizing):
+    result = is_game_over(board)
+    if result == 'X':
+        return -1
+    if result == 'O':
+        return 1
+    if result == 'Tie':
+        return 0
 
-state = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-depth = 9
-isMaxPlayer = True
+    if is_maximizing:
+        best_score = -float("inf")
+        for i in range(3):
+            for j in range(3):
+                if board[i][j] == ' ':
+                    board[i][j] = 'O'
+                    score = minimax(board, depth + 1, False)
+                    board[i][j] = ' '
+                    best_score = max(score, best_score)
+        return best_score
+    else:
+        best_score = float("inf")
+        for i in range(3):
+            for j in range(3):
+                if board[i][j] == ' ':
+                    board[i][j] = 'X'
+                    score = minimax(board, depth + 1, True)
+                    board[i][j] = ' '
+                    best_score = min(score, best_score)
+        return best_score
 
 while True:
-    player_result = check_winner(state)
-    if player_result == 1:
-        print("Player wins!")
-        break
-    elif check_tie(state):
-        print("It's a tie!")
-        break
-    else:
-        depth = player_ply(depth)
+    print_board(board)
+    make_move('X', board)
 
-    bot_result = check_winner(state)
-    if bot_result == -1:
-        print("Bot wins!")
+    result = is_game_over(board)
+    if result:
+        print_board(board)
+        if result == 'Tie':
+            print("It's a tie!")
+        else:
+            print(f"Player {result} wins!")
         break
-    elif check_tie(state):
-        print("It's a tie!")
+
+    bot_move(board)
+
+    result = is_game_over(board)
+    if result:
+        print_board(board)
+        if result == 'Tie':
+            print("It's a tie!")
+        else:
+            print(f"Player {result} wins!")
         break
-    else:
-        depth = bot_ply(depth)
